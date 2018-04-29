35ec05dc9c0282677c9431ad733f956c9a66ff5c
Wyrine <kashahat@gmail.com>
2018-04-29 18:44:31 -0400

    changed folds and add woPre file

--- data/splitter.py
+++ data/splitter.py
@@ -5,5 +5,5 @@ a = np.arange(12811)
 np.random.shuffle(a)
 
 for i in range(0, 12811, 557):
-    s  = ",".join(a[i : min(i+557, 12810) ].astype(str))
-    print s
+    s  = " ".join(a[i : min(i+557, 12810) ].astype(str))
+    print(s)
