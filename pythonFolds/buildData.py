d0f4c072cacb3f25ce996f3b10d97b28e4dce2c7
Wyrine <kashahat@gmail.com>
2018-05-04 23:33:06 -0400

    finished buildData.py

--- pythonFolds/buildData.py
+++ pythonFolds/buildData.py
@@ -4,6 +4,7 @@ import numpy as np
 import sys
 
 def buildData(fName):
+		""" reads the data file and return the data set and it's classes """
 		with open(fName) as fin:
 				fin.readline()
 				rv = []
@@ -11,5 +12,6 @@ def buildData(fName):
 						try:
 								rv.append([float(i) for i in line.replace("\n", "").split(",")])
 						except:
-								print(line)
-		return np.array(rv)
+								pass
+		rv = np.array(rv)
+		return rv[:, :-1], rv[:, -1]
