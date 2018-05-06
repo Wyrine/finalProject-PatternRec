8fb6dbc8bb1081b5bb80ec4cbc87b0e76d348b1d
Wyrine <kashahat@gmail.com>
2018-05-06 19:06:39 -0400

    added evaluation.py

---
+++ src/evaluation.py
@@ -0,0 +1,41 @@
+#!/usr/local/bin/python3
+import numpy as np
+
+def rocData(conMat):
+		rv = {}
+		tp, tn, fp, fn = conMat[1,1], conMat[0,0], conMat[0,1], conMat[1,0]
+		rv["TN"] =  tn
+		rv["TP"] = tp
+		rv["FP"] = fp 
+		rv["FN"] = fn
+		rv["Acc"] = (tp+tn)/(tp+tn+fp+fn)
+		rv["Sens"] = tp / (tp+fn)
+		rv["Spec"] = tn / (tn+fp)
+		rv["Prec"] = tp / (tp+fp)
+		return rv
+		
+def getAvgProbMatrix(normMat):
+		rv = np.zeros((2,2), dtype=np.float64)
+		for mat in normMat:
+				rv += mat
+		return (rv / 23)
+
+def buildConfusionMatrices(results):
+		"""
+				goes through the results 
+		"""
+		rv = []
+		for pred, ground in results:
+				tmp = np.zeros((2,2), dtype = np.int)
+				for x, y in zip(pred, ground):
+						tmp[x, y] += 1
+				rv.append(tmp)
+		return rv
+
+def normalizeConfMat(conMat):
+		for i, mat in enumerate(conMat):
+				divisor = 0
+				for a in conMat:
+						divisor += np.sum(a)
+				conMat[i,:] /= divisor
+		return conMat
